27d93d4b33c948302e817f0ba44cffa95bf19acf
martin <admin@crossleys.biz>
2023-01-24 19:36:14 +0000

    add loop counter

--- main.c
+++ main.c
@@ -11,11 +11,12 @@ int main() {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
+    int i=0;
     while(1) {
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
         gpio_put(LED_PIN, 1);
-        puts("Hello World\n");
+        printf("Hello World %d\n", i++);
         sleep_ms(1000);
     }
 }
